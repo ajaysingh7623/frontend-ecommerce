b89bb9217dbaacd598cf39167a44c12ec640c327
ajay singh <you@example.com>
2020-08-30 09:55:46 -0700

    push to git

---
+++ src/core/about.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import "../about.css"; 
+
+
+const About=()=>{
+    return(
+        <>
+        <body>
+        <header class='masthead'>
+  <p class='masthead-intro'>Hi I'm</p>
+  <h1 class='masthead-heading'>Ajay Singh!</h1>
+  
+
+  <section class="introduction-section">
+    <h1>.</h1>
+    <p>.</p>
+    <p>.</p>
+</section>
+<section class="location-section">
+    <h1>.</h1>
+    <p>. </p>
+</section>
+<section class="questions-section">
+    <h1>.</h1>
+    <h2>.</h2>
+    <p>.</p>
+    <h2>.</h2>
+    <p>.</p>
+    <h2>.</h2>
+    <p>.</p>
+    <h2>.</h2>
+    <p>.</p>
+</section>
+
+
+
+<footer class="content-footer">
+    <p>Say hi to me on these social networks.</p>
+    <ul class="social">
+        <li><a href="https://github.com/ajaysingh7623" class="fa fa-github"></a></li>
+        <li><a href="https://www.instagram.com/ajay_singh_cs/?hl=en" class="fa fa-instagram"></a></li>
+        <li><a href="https://www.linkedin.com/in/ajay-singh-3b1789114/" class="fa fa-linkedin"></a></li>
+    </ul>
+    
+</footer>
+</header>
+</body>
+
+</>
+    );
+}
+
+export default About;
